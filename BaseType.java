0ce2cfad055540897dbfa7852bb410d2ffc63c98
jxtxzzw <jxtxzzw@126.com>
2019-10-03 22:43:42 +0800

    Add some interfaces, abstract classes, and simple classes

---
+++ BaseType.java
@@ -0,0 +1,17 @@
+public class BaseType {
+    private String name;
+    private String code;
+
+    public BaseType(String name, String code) {
+        this.name = name;
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
